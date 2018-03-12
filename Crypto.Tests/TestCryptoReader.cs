6283797327aa49d2a17d953f54bc750bc51113ac
xjohnwu <xjohnwu@gmail.com>
2018-03-12 21:29:39 +0000

    show file

--- Crypto.Tests/TestCryptoReader.cs
+++ Crypto.Tests/TestCryptoReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Crypto.Lib;
@@ -29,23 +30,25 @@ namespace Crypto.Tests
         public void Get_All_HistoricalData()
         {
             var reader = new CryptoReader();
-            var df = Frame.FromColumns(CoinPrices(reader, reader.GetCoinList().Select(c=>c.Symbol)));
+            var df = Frame.FromColumns(CoinPrices(reader, reader.GetCoinList().Select(c=>c.Symbol), new DateTime(2017, 10, 10)));
             df.SaveCsv(Path.Combine(@"G:\temp\Crypto", DateTime.Now.ToString("yyyyMMdd_HHmmss_ffffff")+ ".csv"), true);
         }
 
-        [TestCase("BTC", "ETH", "IOT", "XRP")]
+        [TestCase("BTC", "ETH", "IOT", "XRP", "EOS")]
         public void Get_HistoricalData(params string[] symbols)
         {
             var reader = new CryptoReader();
-            var df = Frame.FromColumns(CoinPrices(reader, symbols));
-            df.SaveCsv(Path.Combine(@"G:\temp\Crypto", DateTime.Now.ToString("yyyyMMdd_HHmmss_ffffff") + ".csv"), true);
+            var df = Frame.FromColumns(CoinPrices(reader, symbols, new DateTime(2017, 10, 10)));
+            var filePath = Path.Combine(@"G:\temp\Crypto", DateTime.Now.ToString("yyyyMMdd_HHmmss_ffffff") + ".csv");
+            df.SaveCsv(filePath, true);
+            Process.Start(filePath);
         }
 
-        private IEnumerable<KeyValuePair<string, Series<DateTime, decimal>>> CoinPrices(CryptoReader reader, IEnumerable<string> symbols)
+        private IEnumerable<KeyValuePair<string, Series<DateTime, decimal>>> CoinPrices(CryptoReader reader, IEnumerable<string> symbols, DateTime fromDate)
         {
             foreach (var symbol in symbols)
             {
-                var data = reader.GetHistoricalData(symbol, "USD", new DateTime(2018, 1, 15), DateTime.UtcNow.Date).ToArray();
+                var data = reader.GetHistoricalData(symbol, "USD", fromDate, DateTime.UtcNow.Date).ToArray();
                 if (!data.Any())
                     continue;
                 yield return KeyValue.Create(symbol, new Series<DateTime, decimal>(data.Select(p => KeyValue.Create(DateHelper.FromEpochTime(p.Time), p.Close))));
